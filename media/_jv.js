09ae9b6d5f1aad97686540876e3505599a454172
huseyinay1010 <huseyinayanoglu@live.de>
2025-08-06 23:21:55 +0200

    init commit
    hi

---
+++ media/_jv.js
@@ -0,0 +1,4 @@
+var token = '';
+var vv_hinweis_anzeigen = true;
+document.getElementById('vv_hinweis_anzeigen').innerHTML = vv_hinweis_anzeigen;
+document.getElementById('token').innerHTML = token;
